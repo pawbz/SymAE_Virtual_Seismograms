db84704795b9c1717c938ec13d4d5e1680f8f988
Pawan Bharadwaj <bharadwaj.pawan@gmail.com>
2022-08-26 11:51:00 +0530

    created index.jl

---
+++ index.jl
@@ -0,0 +1,31 @@
+### A Pluto.jl notebook ###
+# v0.19.11
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ a9fb1094-2506-11ed-1e18-f903543c0ec7
+md"""
+## Learning earthquake sources using symmetric autoencoders
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.8.0"
+manifest_format = "2.0"
+project_hash = "da39a3ee5e6b4b0d3255bfef95601890afd80709"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╠═a9fb1094-2506-11ed-1e18-f903543c0ec7
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
